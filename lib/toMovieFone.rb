0e73255ad174952753334a98c5c46e15fddc1d2f
Elias <eliasiscoding@gmail.com>
2020-02-25 19:18:38 -0500

    Initial Commit

---
+++ lib/toMovieFone.rb
@@ -0,0 +1,6 @@
+require "toMovieFone/version"
+#testing
+module ToMovieFone
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/toMovieFone/version.rb
@@ -0,0 +1,3 @@
+module ToMovieFone
+  VERSION = "0.1.0"
+end
